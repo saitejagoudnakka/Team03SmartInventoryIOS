7636ced32a23800e9175ffdbdf694a1abec54188
HariniBeeram <S565877@nwmissouri.edu>
2024-05-20 12:00:04 -0700

    Updated UI

---
+++ Smart Inventory/Smart Inventory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Smart Inventory
+//
+//  Created by Santosh Reddy Gurram on 5/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Smart Inventory/Smart InventoryUITests/Smart_InventoryUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Smart_InventoryUITestsLaunchTests.swift
+//  Smart InventoryUITests
+//
+//  Created by Santosh Reddy Gurram on 5/14/24.
+//
+
+import XCTest
+
+final class Smart_InventoryUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
