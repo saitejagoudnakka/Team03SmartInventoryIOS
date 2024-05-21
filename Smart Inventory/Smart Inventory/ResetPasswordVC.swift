0c0598a5b82b8f5e971e1504aaf71ea24977de55
HariniBeeram <S565877@nwmissouri.edu>
2024-05-21 10:36:00 -0700

    Updated login , reset , and account creation

---
+++ Smart Inventory/Smart Inventory/ResetPasswordVC.swift
@@ -0,0 +1,29 @@
+//
+//  ResetPasswordVC.swift
+//  Smart Inventory
+//
+//  Created by Harini Beeram on 5/21/24.
+//
+
+import UIKit
+
+class ResetPasswordVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
