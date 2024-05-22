24d6c18041519b6d61991dc3cf52cf533e1d6500
HariniBeeram <S565877@nwmissouri.edu>
2024-05-22 11:46:38 -0700

    updated Login credentials

--- Smart Inventory/Smart Inventory/ResetPasswordVC.swift
+++ Smart Inventory/Smart Inventory/ResetPasswordVC.swift
@@ -10,44 +10,57 @@ import UIKit
 import FirebaseAuth
 
 class ResetPasswordVC: UIViewController {
-
-    @IBOutlet weak var email: UITextField!
     
-    
-    
-    @IBAction func onSend(_ sender: UIButton) {
+    override func viewDidLoad() {
+           super.viewDidLoad()
+           self.view.backgroundColor = UIColor.systemGray6
+       }
         
         
-        guard let email = email.text, !email.isEmpty else {
-                    showAlert(message: "Please enter your email address.")
-                    return
-                }
-
-                Auth.auth().sendPasswordReset(withEmail: email) { error in
-                    DispatchQueue.main.async {
-                        if let error = error {
-                            self.showAlert(message: "Error resetting password: \(error.localizedDescription)")
-                        } else {
-                            self.showAlert(message: "Reset email sent successfully.")
+        
+        
+        
+        @IBOutlet weak var email: UITextField!
+        
+        
+        
+        @IBAction func onSend(_ sender: UIButton) {
+            
+            guard let email = email.text, !email.isEmpty else {
+                        showAlert(message: "Please enter your email address.")
+                        return
+                    }
+                    
+                    Auth.auth().sendPasswordReset(withEmail: email) { error in
+                        DispatchQueue.main.async {
+                            if let error = error {
+                                self.showAlert(message: "Error resetting password: \(error.localizedDescription)")
+                            } else {
+                                self.showAlert(message: "A password reset email has been sent to \(email). Please check your inbox and follow the instructions to reset your password.")
+                                //self.performSegue(withIdentifier: "resettologinSegue", sender: nil)
+                            }
                         }
                     }
+           
+        }
+                
+                private func showAlert(message: String) {
+                    let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                    present(alert, animated: true, completion: nil)
                 }
             }
 
-            private func showAlert(message: String) {
-                let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-                present(alert, animated: true, completion: nil)
-            }
-        }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension UITextField {
+    func setLeftPaddingPoints(_ amount: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
     }
-    */
-
-   
+    
+    func setRightPaddingPoints(_ amount: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.height))
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
+}
